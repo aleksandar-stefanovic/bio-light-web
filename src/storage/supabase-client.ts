dd8b280bc879f73d2f39f164ac7441a2655e3a29
Aleksandar Stefanović <stefanoviczaleksandar@gmail.com>
2024-09-25 09:08:31 +0200

    Remove duplicate Supabase client

--- src/storage/supabase-client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient('https://xmpstgdxhdjllxqykguf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcHN0Z2R4aGRqbGx4cXlrZ3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk0NzczODIsImV4cCI6MTk4NTA1MzM4Mn0.OnqC9ZudnUmr1_wg6j_fpFpsGCJLp631JTaVAa4klK0')
-
-export default supabase
--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,3 +1,4 @@
 import {createClient} from '@supabase/supabase-js';
 
-export default createClient('https://xmpstgdxhdjllxqykguf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcHN0Z2R4aGRqbGx4cXlrZ3VmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2OTQ3NzM4MiwiZXhwIjoxOTg1MDUzMzgyfQ.UMpHrmIuFMCAhjyOxWQhn2Fc4fxPUOkwhtGfS5uEA7A');
+const publicAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcHN0Z2R4aGRqbGx4cXlrZ3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMTI1MDksImV4cCI6MjA0Mjc4ODUwOX0.fpq6nqbzwOIyyknMNp0sraN1hQEqZDyEg9O97WPrftw';
+export default createClient('https://xmpstgdxhdjllxqykguf.supabase.co', publicAnonKey);
